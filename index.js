6275cdcaec95bcfb6084cd3d584c5d6f64c986af
ismayilovfd <sd7njhqc6@code.edu.az>
2023-11-22 22:19:40 -0800

    change

--- index.js
+++ index.js
@@ -27,30 +27,61 @@
                 
 
 
-// const username = document.getElementById("inpUsername");
-// const email = document.getElementById("inpEmail");
-// const password = document.getElementById("inpPassword");
-// const address = document.getElementById("inpAddress");
-// const country = document.getElementById("inpCountry");
-// const submit = document.getElementById("formTwo");
+const username = document.getElementById("inpUsername");
+const email = document.getElementById("inpEmail");
+const password = document.getElementById("inpPassword");
+const address = document.getElementById("inpAddress");
+const country = document.getElementById("inpCountry");
+const submit = document.getElementById("formTwo");
 
-// function PostData (event) {
+function PostData (event) {
 
-//             event.preventDefault(axios.post("https://northwind.vercel.app/api/customers", {
+            event.preventDefault(axios.post("https://northwind.vercel.app/api/customers", {
                 
-//                 username: username.value,
-//                 email: email.value,
-//                 password: password.value,
-//                 address: address.value,
-//                 country: country.value,
+                username: username.value,
+                email: email.value,
+                password: password.value,
+                address: address.value,
+                country: country.value,
                 
-//             }).then(res => {
-//                     console.log(res);
+            }).then(res => {
+                    console.log(res);
     
-//                     submit.reset();
+                    submit.reset();
     
-//                 }));
+                }));
                 
-//                     }
+                    }
                     
-//                     submit.addEventListener('submit', PostData)
+                    submit.addEventListener('submit', PostData)
+
+
+
+
+// const name = document.getElementById("inpName");
+// const surname = document.getElementById("inpSurname");
+// const fathername = document.getElementById("inpFathername");
+// const fin = document.getElementById("inpFin");
+// const email = document.getElementById("inpEmail");
+// const submit = document.getElementById("form-data");
+
+// function Datapost (event) {
+
+//                 event.preventDefault(axios.post("https://northwind.vercel.app/api/customers", {
+                    
+//                     name: name.value,
+//                     surname: surname.value,
+//                     fathername: fathername.value,
+//                     fin: fin.value,
+//                     email: email.value,
+                    
+//                 }).then(res => {
+//                         console.log(res);
+        
+//                         submit.reset();
+        
+//                     }));
+                    
+//                         }
+                        
+//                         submit.addEventListener('submit', Datapost)
